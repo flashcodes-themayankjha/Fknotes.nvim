fd7e2e419d2006a76582b434103ef655fbb6647d
flashcodes-themayankjha <the.mayank.k.jha@gmail.com>
2025-07-17 22:23:52 +0530

    Improved Menu ui for FkNotes

--- lua/fknotes/ui/menu.lua
+++ lua/fknotes/ui/menu.lua
@@ -4,19 +4,21 @@ function M.open()
   local buf = vim.api.nvim_create_buf(false, true)
 
   local menu_lines = {
-    "FKTODO MENU",
+    "  🔹 FKTASK MENU 🔹",
     "",
-    "1. Create Task",
-    "2. Browse Tasks",
-    "3. Exit"
+    "  1. ➕ Create Task",
+    "  2. 📋 Browse Tasks",
+    "  3. ❌ Exit",
   }
 
   vim.api.nvim_buf_set_lines(buf, 0, -1, false, menu_lines)
   vim.api.nvim_buf_set_option(buf, 'modifiable', false)
+  vim.api.nvim_buf_set_option(buf, 'buftype', 'prompt')
+  vim.api.nvim_buf_set_option(buf, 'filetype', 'fknotes_menu')
 
-  local width = 30
+  local width = 40
   local height = #menu_lines
-  local opts = {
+  local win_opts = {
     style = "minimal",
     relative = "editor",
     width = width,
@@ -26,11 +28,36 @@ function M.open()
     border = "rounded",
   }
 
-  local win = vim.api.nvim_open_win(buf, true, opts)
+  local win = vim.api.nvim_open_win(buf, true, win_opts)
 
-  -- Simple key mappings for demo
-  vim.api.nvim_buf_set_keymap(buf, 'n', '1', [[:lua require("fknotes.ui.task_form").new_task()<CR>]], { noremap = true, silent = true })
-  vim.api.nvim_buf_set_keymap(buf, 'n', '3', ':bd!<CR>', { noremap = true, silent = true }) -- Exit
+  -- Highlight current line
+  vim.cmd("setlocal cursorline")
+
+  -- Key: Enter to select option
+  vim.api.nvim_buf_set_keymap(buf, 'n', '<CR>', '', {
+    noremap = true,
+    silent = true,
+    callback = function()
+      local line = vim.api.nvim_get_current_line()
+
+      if line:match("Create Task") then
+        vim.api.nvim_win_close(win, true)
+        require("fknotes.ui.task_form").new_task()
+
+      elseif line:match("Browse Tasks") then
+        vim.api.nvim_win_close(win, true)
+        -- require("fknotes.ui.task_browser").open()
+        vim.notify("📂 Browse Tasks – Not implemented yet", vim.log.levels.INFO)
+
+      elseif line:match("Exit") then
+        vim.api.nvim_win_close(win, true)
+      end
+    end,
+  })
+
+  -- Optional: j/k to navigate
+  vim.api.nvim_buf_set_keymap(buf, 'n', 'j', 'j', { noremap = true, silent = true })
+  vim.api.nvim_buf_set_keymap(buf, 'n', 'k', 'k', { noremap = true, silent = true })
 end
 
 return M
