310778551d03000d272fbb798037f1638cc3bd7f
flashcodes-themayankjha <the.mayank.k.jha@gmail.com>
2025-08-07 11:57:50 +0530

    Feat: New Command Buttons

--- lua/fknotes/config.lua
+++ lua/fknotes/config.lua
@@ -1,5 +1,8 @@
 vim.cmd([[
   highlight FkNotesRedButton guifg=#f7768e gui=bold
   highlight FkNotesBlueButton guifg=#7dcfff gui=bold
+  highlight link FknotesTitle Title
+  highlight link FknotesComment Comment
+  highlight FkButton guifg=#00afff gui=bold
 ]])
 
